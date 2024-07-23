aa0531a4318fa919a6c29dcba07fa2e1fd89f893
theaavashhh <aavash.sthaa@gmail.com>
2024-07-23 23:02:20 +0545

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func deposit() {
+	fmt.Println("Deposite amount...")
+}
+
+func withDraw() {
+	fmt.Println("Withdrawing amount...")
+}
+
+func main() {
+	deposit()
+
+}
+
